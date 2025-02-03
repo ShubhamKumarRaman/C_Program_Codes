ff760d21df985e760f400666fdbeb26d1af742ce
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-03 13:21:05 +0530

    Create 22CommaOperator.c

---
+++ 01BasicPrograms/22CommaOperator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    a=2,3,4;
+    b=(3,5,7);
+    printf("\nValue of a is %d", a);
+    printf("\nValue of b is %d", b);
+    getch();
+    return 0;
+}
