83bfb95f3f20cd6d06e373a77848d1006a1eb884
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-11 08:14:15 +0530

    Prime Number

---
+++ 03Loops/04PrimeWhile.c
@@ -0,0 +1,10 @@
+// Write a progam to determine whether a number is prime or not. A prime number is one, which is divisible only by 1 or itself.
+#include <stdio.h>
+void main()
+{
+    int num;
+    int i =0;
+    while(i<num){
+        
+    }
+}
