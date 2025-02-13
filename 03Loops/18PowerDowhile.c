046615d17eacd6e48bdd904cbc571d2751fe29af
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-13 18:55:50 +0530

    Create 18PowerDowhile.c

---
+++ 03Loops/18PowerDowhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num, power, value = 1;
+    printf("\nEnter a number:");
+    scanf("%d", &num);
+    printf("\nEnter the power:");
+    scanf("%d", &power);
+    int i = 1;
+    do
+    {
+        value = value * num;
+        i++;
+    } while (i <= power);
+    printf("\n%d raised to the power %d : %d", num, power, value);
+    getch();
+    return 0;
+}
