bbbca0193bcb9dece52ea3c19da6f7339c7c7042
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-15 10:17:15 +0530

    Create 26MatchStickGameFor.c

---
+++ 03Loops/26MatchStickGameFor.c
@@ -0,0 +1,32 @@
+// Matchstick game
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int match = 21, user, computer;
+    for (;;)
+    {
+        printf("\nTotal numbre of matchstick left: %d", match);
+        printf("\n---------------------------");
+        printf("\nPick up matchstick (1 to 4):");
+        scanf("%d", &user);
+        if (user > 4)
+        {
+            printf("\nInvalid Entry!");
+            continue;
+        }
+        match = match - user;
+        printf("\nNumber of matchstick left: %d", match);
+        computer = 5 - user;
+        printf("\nComputer picked up %d matchsticks.", computer);
+        match = match - computer;
+        if (match == 1)
+        {
+            printf("\nNumber of matchsticks left:%d", match);
+            printf("\nYou lost the game!");
+            break;
+        }
+    }
+    getch();
+    return 0;
+}
