931db8131e52d3dc093180d41276765dd091bee1
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-21 08:29:05 +0530

    Create 34SizeofVariables.c

---
+++ 01BasicPrograms/34SizeofVariables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    printf("\nSize of char    :%u bytes", sizeof(char));
+    printf("\nSize of int     :%u bytes", sizeof(int));
+    printf("\nSize of float   :%u bytes", sizeof(float));
+    printf("\nSize of long    :%u bytes", sizeof(long));
+    printf("\nSize of short   :%u bytes", sizeof(short));
+    printf("\nSize of double  :%u bytes", sizeof(double));
+    printf("\nSize of long double :%u bytes", sizeof(long double));
+    printf("\nSize of long long   :%u bytes", sizeof(long long));
+    
+}
